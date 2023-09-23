88b4b28b27f2b39ddcaa611aa94fd3dbea2f4dd6
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2023-09-23 21:44:45 +0200

    First look on Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts.jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      "twitter-blue": "#1DA1F2",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
